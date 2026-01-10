0914d1ed81c3f1556540922fbf776183fd94e58e
corbaz <julio.corbaz@gmail.com>
2026-01-10 16:30:41 -0300

    svg youtube

--- card.js
+++ card.js
@@ -20,7 +20,13 @@ function renderVideoCard({ videoUrl = "", thumbnail = "", title = "video", origi
   const clickHandler = `onclick="if(window.startSmartDownload) { window.startSmartDownload(decodeURIComponent('${encodedUrl}'), decodeURIComponent('${encodedFilename}')); } else { console.error('SmartDownload not found'); }"`;
 
   // Render Play Icon only if it is NOT an image
-  const playIconHtml = (type === 'image') ? '' : '<div class="card-play-icon" style="pointer-events: none;">▶️</div>';
+  const playIconHtml = (type === 'image') ? '' : `
+    <button class="ytp-large-play-button ytp-button ytp-large-play-button-red-bg" aria-label="Reproducir" title="Reproducir" style="pointer-events: none;">
+      <svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%">
+        <path class="ytp-large-play-button-bg" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f03"></path>
+        <path d="M 45,24 27,14 27,34" fill="#fff"></path>
+      </svg>
+    </button>`;
 
   if (thumbnail) {
     return `
@@ -47,7 +53,7 @@ function renderVideoCard({ videoUrl = "", thumbnail = "", title = "video", origi
             <video src="${safeVideoUrl}#t=0.01" preload="metadata" muted playsinline
                    class="card-video-element" style="pointer-events: none;">
             </video>
-             <div class="card-play-icon" style="pointer-events: none;">▶️</div>
+             ${playIconHtml}
           </div>
         </div>
       `;
